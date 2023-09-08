d3e9823158fe4f70d608956ea566a8580d3d9cf9
bollic <48652894+bollic@users.noreply.github.com>
2023-09-08 14:51:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT||4000
 
 const cors = require('cors');
 server.use(cors({
-    origin: '*', // use your actual domain name (or localhost), using * is not recommended
+    origin: 'http://localhost:4000', // use your actual domain name (or localhost), using * is not recommended
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
