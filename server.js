ff94f96493dde3b4a5b1088498171a81726ec3cc
bollic <48652894+bollic@users.noreply.github.com>
2023-09-20 18:54:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 // server.js
 const jsonServer = require("json-server");
-// Import the library
-const cors = require('cors');
 const server = jsonServer.create();
 
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT||4000
 
-// Set up a domainList and check against it:
-
-
-server.use(cors(  
-  origin: '*', //['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'],use your actual domain name (or localhost), using * is not recommended
+const cors = require('cors');
+server.use(cors({
+   
+  origin:  ['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'], // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
    credentials: true
-
-}  ))
+}))
 const router = jsonServer.router("db.json");
 
 server.use(middlewares)
