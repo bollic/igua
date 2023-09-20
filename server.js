86acafc8a3fba089c62a422aa3558c9332df1528
bollic <48652894+bollic@users.noreply.github.com>
2023-09-20 18:14:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 // server.js
-//const express = require("express");
 const jsonServer = require("json-server");
-// Import the library:
-//const cors = require('cors');
-
 const server = jsonServer.create();
+
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT||4000
-//const server = express();
-/*
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*', 'http://localhost:4000')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})*/
 
-
-
+const cors = require('cors');
 server.use(cors({
    
-    origin: '*', ['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'],  use your actual domain name (or localhost), using * is not recommended
-   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
+  origin:  ['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'], // use your actual domain name (or localhost), using * is not recommended
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+   credentials: true
 }))
-/*
-server.get("/json", (req, res) => {
-   res.json({ message: "Hello world" });
-});*/
-//const router = express.Router("db.json");
-
 const router = jsonServer.router("db.json");
-
 
 server.use(middlewares)
 server.use(router)
