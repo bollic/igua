767371f4cc5d01f407e633b168bd872ee3cb2bd4
bollic <48652894+bollic@users.noreply.github.com>
2023-09-20 18:32:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT||4000
 
 server.use(cors({
    
-  origin:  ['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'], // use your actual domain name (or localhost), using * is not recommended
+  origin: '*', //['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'],use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
    credentials: true
