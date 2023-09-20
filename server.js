7aa32e520c7cf0f030eb6397c09322f1a0dddbfd
bollic <48652894+bollic@users.noreply.github.com>
2023-09-20 13:20:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 
 //const server = jsonServer.create();
 const server = express();
+/*
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4000')
   res.header('Access-Control-Allow-Headers', '*')
   next()
-})
+})*/
 
 
 /*
