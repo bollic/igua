94b820ed13081cb750beb989b2949382432b7342
bollic <48652894+bollic@users.noreply.github.com>
2023-09-20 12:44:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const cors = require('cors');
 
 const server = jsonServer.create();
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
+
+
 server.use(cors({
    
     origin: '*',// ['localhost:4000', 'https://wild-gray-beaver-robe.cyclic.cloud'],  use your actual domain name (or localhost), using * is not recommended
