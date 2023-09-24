d25e68cf5c6c8c95fe4ff41f6aef4c8e15154993
bollic <48652894+bollic@users.noreply.github.com>
2023-09-24 11:34:36 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -82,7 +82,7 @@ const renderPosts = async (term) => {
 //fetch('https://cors-anywhere.herokuapp.com/https://jsonplaceholder.typicode.com/posts');
  //https://wild-gray-beaver-robe.cyclic.cloud
 // my-json-server.typicode.com/user/repo/posts/1
-let uri = 'https://my-json-server.typicode.com/bollic/iguaa/posts?_sort=data&_order=desc';
+let uri = 'https://my-json-server.typicode.com/bollic/iguaa/posts?_sort=data&_order=esc';
 // https://my-json-server.typicode.com/bollic/iguaa/posts
 //https://wild-gray-beaver-robe.cyclic.cloud/posts?_sort=likes&_order=desc
 // https://github.com/bollic/iguaa/blob/main/db.json
