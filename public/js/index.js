f692a6055c53bbbe966cdb3941deefa7024a4ef3
bollic <48652894+bollic@users.noreply.github.com>
2024-04-19 20:23:37 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -84,7 +84,8 @@ const renderPosts = async (term) => {
 // my-json-server.typicode.com/user/repo/posts/1
 //let uri = 'http://libriecopertine.altervista.org/db.json';
 //let uri =  https://my-json-server.typicode.com/bollic/iguaa/posts
-let uri = 'http://localhost:4000/posts';
+ // https://average-cape-pig.cyclic.cloud
+let uri = 'https://average-cape-pig.cyclic.cloud/posts';
 //let uri =  https://github.com/bollic/iguaa/blob/main/db.json
   if (term) {
     uri += `&q=${term}`
