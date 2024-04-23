f99d342aeabf36b78b47c4f4c0b2ecf890abda6a
bollic <48652894+bollic@users.noreply.github.com>
2024-04-23 20:21:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT||4000
 
-app.use((req, res, next) => {
+server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
