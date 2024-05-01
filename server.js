1c439924aa19ef341d924beaebc8970a767acd41
bollic <48652894+bollic@users.noreply.github.com>
2024-05-01 20:14:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT||3000
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000/','https://average-cape-pig.cyclic.app');
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000/posts','https://average-cape-pig.cyclic.app');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
