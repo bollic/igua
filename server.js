f4f0744234a3e16950a5d391730b1dc73f089da9
bollic <48652894+bollic@users.noreply.github.com>
2024-05-03 14:23:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT||3000
+const cors = require('cors');
 
-server.use((req, res, next) => {
+server.use(cors(req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','http://localhost:3000','https://average-cape-pig.cyclic.app');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
 /*
-const cors = require('cors');
+
 server.use(cors({
  //  https://average-cape-pig.cyclic.app/
   origin:  ['localhost:4000', 'https://average-cape-pig.cyclic.cloud', '*'], // use your actual domain name (or localhost), using * is not recommended
