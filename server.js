e3c6c3b854d7c943a782ecdc440f32202d591de3
bollic <48652894+bollic@users.noreply.github.com>
2024-05-03 14:38:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const port = process.env.PORT||3000
 const cors = require('cors');
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000','https://average-cape-pig.cyclic.app');
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000','*');
+     res.header('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
