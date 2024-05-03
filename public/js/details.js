da4663b22887f3bcdf783c671fc6eac817fde8e4
bollic <48652894+bollic@users.noreply.github.com>
2024-05-03 19:33:47 +0200

    Update details.js

--- public/js/details.js
+++ public/js/details.js
@@ -5,7 +5,7 @@ const deleteBtn = document.querySelector('.delete');
 
 const renderDetails = async () => {
 
-  const res = await fetch('https://average-cape-pig.cyclic.app/posts/' + id);
+  const res = await fetch('https://igua.onrender.com/posts/' + id);
   if (!res.ok) {
 
     window.location.replace('/');
@@ -22,7 +22,7 @@ const renderDetails = async () => {
 }
 
 deleteBtn.addEventListener('click', async () => {
-  const res = await fetch('https://average-cape-pig.cyclic.app/posts/' + id, {
+  const res = await fetch('https://igua.onrender.com/posts/' + id, {
     method: 'DELETE'
   });
  window.location.replace("/");
