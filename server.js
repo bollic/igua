4f7031d0b9dfb60673f51052ff74145c45ed54c2
bollic <48652894+bollic@users.noreply.github.com>
2024-06-02 15:14:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,66 @@
 // server.js
+//nella console: npm start 
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Thing = require('./models/Thing');
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+
+
+// JSON Server is running on port 4000
+//posso vedere il BACKEND , cioe' il json su http://localhost:4000/posts?_sort=likes&_order=desc
+//per vedere , il FRONTEND ,vai su index.html open with live server
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT||8080
-const cors = require('cors');
+const port = process.env.PORT||3001
+/*server.all('/', (req, res) => {
+  req.headers['origin'] || req.get('Origin')
+  console.log(['origin'])
+  res.get('Origin')
+  res.send('Yo!')
+})*/
+
+
+mongoose.connect('mongodb+srv://soniaBoss:KLP59dnH8@cluster0.cvr9g5a.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Add custom middleware for CORS
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000','*');
-     res.header('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', 'https://average-cape-pig.cyclic.app/'); // Allow any origin
+  res.header('Access-Control-Allow-Origin',  req.headers['origin'] || req.get('Origin') ); // Allow any domain coming in Origin header.
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+router.get('/posts', (req, res) =>{
+  res.send(posts);
+});
 /*
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
 
-server.use(cors({
- //  https://average-cape-pig.cyclic.app/
-  origin:  ['localhost:4000', 'https://average-cape-pig.cyclic.cloud', '*'], // use your actual domain name (or localhost), using * is not recommended
+app.use(cors({
+  origin:  ['*','http://localhost:8000', 'http://localhost:4000/posts'], // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+   allowedHeaders: ['Content','Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+   allowedOrigin: ['*'],
+   
    credentials: true
-}))*/
-
+}))
+*/
 
 server.use(middlewares)
 server.use(router)
