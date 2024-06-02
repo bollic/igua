22ec07758e626840111cedcc2ddc94860de198f6
bollic <48652894+bollic@users.noreply.github.com>
2024-06-02 15:32:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
+
 const mongoose = require('mongoose');
 const Thing = require('./models/Thing');
-
+const cors = require('cors');
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: true}))
 
@@ -35,7 +35,7 @@ mongoose.connect('mongodb+srv://soniaBoss:KLP59dnH8@cluster0.cvr9g5a.mongodb.net
 
 // Add custom middleware for CORS
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://average-cape-pig.cyclic.app/'); // Allow any origin
+  res.header('Access-Control-Allow-Origin', 'https://igua.onrender.com'); // Allow any origin
   res.header('Access-Control-Allow-Origin',  req.headers['origin'] || req.get('Origin') ); // Allow any domain coming in Origin header.
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -47,13 +47,13 @@ router.get('/posts', (req, res) =>{
 });
 /*
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001', 'http://localhost:3001')
   res.header('Access-Control-Allow-Headers', '*')
   next()
 })
 
 app.use(cors({
-  origin:  ['*','http://localhost:8000', 'http://localhost:4000/posts'], // use your actual domain name (or localhost), using * is not recommended
+  origin:  ['*', 'http://localhost:3001/posts'], // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
    allowedHeaders: ['Content','Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
    allowedOrigin: ['*'],
