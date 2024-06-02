75f69331696ee8c0fdc259000a7c653d47d00647
bollic <48652894+bollic@users.noreply.github.com>
2024-06-02 14:28:37 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -54,8 +54,8 @@ afficherFilms()
 // Specify the API endpoint for user data
 
 button2Filtered.addEventListener("click", function() {
-  apiUrl = 'http://igua.onrender.com/posts';
-  const URL = "http://igua.onrender.com/posts";
+  apiUrl = 'https://igua.onrender.com/posts';
+  const URL = "https://igua.onrender.com/posts";
   let template = '';
 //const sidebar = document.getElementByClass("sidebar");
 sidebar.innerHTML = "<p>Loading...";
@@ -146,7 +146,7 @@ const names = posts.filter(post => post.category === "content")
   })
 
 moyenFiltered.addEventListener("click", function() {
-apiUrl = 'http://localhost:3001/posts?category=moyen';
+apiUrl = 'https://igua.onrender.com/posts?category=moyen';
 // Make a GET request using the Fetch API
 fetch(apiUrl)
   .then(response => {
